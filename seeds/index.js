9555d41937353db97ebd3bd5748b7ae111bb56c0
Goran <stoykov.g@outlook.de>
2023-02-08 21:50:11 +0100

    Section 41: Adding Images

--- seeds/index.js
+++ seeds/index.js
@@ -17,9 +17,14 @@ const seedDB = async () => {
   await Campground.deleteMany({});
   for (let i = 0; i < 50; i++) {
     const random1000 = Math.floor(Math.random() * 1000);
+    const price = Math.floor(Math.random() * 20) + 10;
     const camp = new Campground({
       location: `${cities[random1000].city}, ${cities[random1000].state}`,
       title: `${sample(descriptors)} ${sample(places)}`,
+      image: "https://source.unsplash.com/collection/483251/",
+      description:
+        "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Excepturi cum, nulla temporibus repellat molestias dolore rem odio commodi asperiores laborum iste optio expedita, deserunt magnam repudiandae accusantium dolorem! Enim, harum.",
+      price,
     });
     await camp.save();
   }
